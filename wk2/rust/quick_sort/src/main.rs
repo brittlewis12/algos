97dde4eef024cf9f5f943a247568168c7e3c389a
Britt Lewis <brittlewis12@gmail.com>
2015-02-10 02:41:42 -0500

    Prefer default integer type (i32) except when indexing array (usize)
    * also total count can't be negative, so u32

--- wk2/rust/quick_sort/src/main.rs
+++ wk2/rust/quick_sort/src/main.rs
@@ -7,12 +7,12 @@ mod quick_sort;
 
 fn main() {
     let mut file = BufferedReader::new(File::open(&Path::new("IntegerInput.txt")));
-    let integers: Vec<usize> = file.lines().enumerate().map(|(i, x)| {
+    let integers: Vec<i32> = file.lines().enumerate().map(|(i, x)| {
         x.unwrap()
         .trim()
-        .parse::<usize>()
+        .parse::<i32>()
         .unwrap_or_else(|err| {
-            panic!("Unable to parse int at line:{}: {}", i + 1us, err)
+            panic!("Unable to parse int at line:{}: {}", i + 1, err)
         })
     }).collect();
 
@@ -21,7 +21,7 @@ fn main() {
     let (_, result1) = quick_sort::count_comparisons(integers.clone());
     println!("Comparisons with first: {}", result1);
 
-    let (_, result2) = quick_sort::count_comparisons_with_pivot(integers.clone(), -1is);
+    let (_, result2) = quick_sort::count_comparisons_with_pivot(integers.clone(), -1);
     println!("Comparisons with last: {}", result2);
 
     let (_, result3) = quick_sort::count_median_comparisons(integers.clone());
--- wk2/rust/quick_sort/src/quick_sort.rs
+++ wk2/rust/quick_sort/src/quick_sort.rs
@@ -1,28 +1,35 @@
 use std::num::Float;
 use std::borrow::ToOwned;
 
-pub fn sort(og_vec: Vec<usize>) -> Vec<usize> {
-    let vec: Vec<usize> = og_vec.to_owned();
-    sort_with_pivot(vec, 0us)
+pub fn sort(og_vec: Vec<i32>) -> Vec<i32> {
+    let vec: Vec<i32> = og_vec.to_owned();
+    sort_with_pivot(vec, 0)
 }
 
-pub fn sort_with_pivot(vec: Vec<usize>, custom_pivot: usize) -> Vec<usize> {
-    if vec.len() <= 1us {
+pub fn sort_with_pivot(vec: Vec<i32>, custom_pivot: i32) -> Vec<i32> {
+    if vec.len() <= 1 {
         return vec
     }
 
-    let result = partition(vec, custom_pivot);
-    let pivot: Vec<usize> = result.pivot;
-    let mut left: Vec<usize> = sort(result.left);
-    let right: Vec<usize> = sort(result.right);
+    let pivot_idx =
+        if custom_pivot == -1i32 {
+            vec.len() - 1
+        } else {
+            custom_pivot as usize
+        };
+
+    let result = partition(vec, pivot_idx);
+    let pivot: Vec<i32> = result.pivot;
+    let mut left: Vec<i32> = sort(result.left);
+    let right: Vec<i32> = sort(result.right);
 
     left.push_all(&pivot);
     left.push_all(&right);
     left
 }
 
-fn partition(og_vec: Vec<usize>, custom_pivot: usize) -> Partition {
-    if og_vec.len() <= 1us {
+fn partition(og_vec: Vec<i32>, custom_pivot: usize) -> Partition {
+    if og_vec.len() <= 1 {
         return Partition {
             left: Vec::new(),
             pivot: og_vec,
@@ -30,54 +37,54 @@ fn partition(og_vec: Vec<usize>, custom_pivot: usize) -> Partition {
         }
     }
 
-    let mut vec: Vec<usize> = og_vec.to_owned();
+    let mut vec: Vec<i32> = og_vec.to_owned();
 
-    if custom_pivot != 0us {
-        vec.swap(0us, custom_pivot);
+    if custom_pivot != 0 {
+        vec.swap(0, custom_pivot);
     }
 
-    let pivot = 0us;
-    let mut i = 1us;
-    let mut j = 1us;
+    let pivot = 0;
+    let mut i = 1;
+    let mut j = 1;
     while j < vec.len() {
         if vec[j] < vec[pivot] {
             if i != j {
                 vec.swap(i, j);
             }
-            j += 1us;
-            i += 1us;
+            j += 1;
+            i += 1;
         } else {
-            j += 1us;
+            j += 1;
         }
     }
-    vec.swap(pivot, i - 1us);
+    vec.swap(pivot, i - 1);
 
     return Partition {
-        pivot: vec![vec[i-1us]],
-        left: vec.iter().enumerate().filter(|&(idx, &_n)| idx < i - 1us).map(|(_idx, &n)| n).collect(),
-        right: vec.iter().enumerate().filter(|&(idx, &_n)| idx > i - 1us).map(|(_idx, &n)| n).collect(),
+        pivot: vec![vec[i-1]],
+        left: vec.iter().enumerate().filter(|&(idx, &_n)| idx < i - 1).map(|(_idx, &n)| n).collect(),
+        right: vec.iter().enumerate().filter(|&(idx, &_n)| idx > i - 1).map(|(_idx, &n)| n).collect(),
     }
 }
 
-pub fn count_comparisons(vec: Vec<usize>) -> (Vec<usize>, usize) {
-    count_comparisons_with_pivot_and_count(vec, 0is, 0us)
+pub fn count_comparisons(vec: Vec<i32>) -> (Vec<i32>, u32) {
+    count_comparisons_with_pivot_and_count(vec, 0i32, 0)
 }
 
-pub fn count_comparisons_with_pivot(vec: Vec<usize>, custom_pivot: isize) -> (Vec<usize>, usize) {
-    count_comparisons_with_pivot_and_count(vec, custom_pivot, 0us)
+pub fn count_comparisons_with_pivot(vec: Vec<i32>, custom_pivot: i32) -> (Vec<i32>, u32) {
+    count_comparisons_with_pivot_and_count(vec, custom_pivot, 0)
 }
 
-pub fn count_comparisons_with_pivot_and_count(og_vec: Vec<usize>, custom_pivot: isize, count: usize) -> (Vec<usize>, usize) {
+pub fn count_comparisons_with_pivot_and_count(og_vec: Vec<i32>, custom_pivot: i32, count: u32) -> (Vec<i32>, u32) {
     let vec = og_vec.to_owned();
-    if vec.len() <= 1us {
+    if vec.len() <= 1 {
         return (vec, count)
     }
 
-    let total = count + vec.len() - 1us;
+    let total = count + vec.len() as u32 - 1;
 
     let pivot_idx =
-        if custom_pivot == -1is {
-            vec.len() - 1us
+        if custom_pivot == -1i32 {
+            vec.len() - 1
         } else {
             custom_pivot as usize
         };
@@ -93,16 +100,16 @@ pub fn count_comparisons_with_pivot_and_count(og_vec: Vec<usize>, custom_pivot: 
     (left, count2)
 }
 
-pub fn count_median_comparisons(vec: Vec<usize>) -> (Vec<usize>, usize) {
-    count_median_comparisons_with_count(vec, 0us)
+pub fn count_median_comparisons(vec: Vec<i32>) -> (Vec<i32>, u32) {
+    count_median_comparisons_with_count(vec, 0)
 }
 
-pub fn count_median_comparisons_with_count(vec: Vec<usize>, count: usize) -> (Vec<usize>, usize) {
-    if vec.len() <= 1us {
+pub fn count_median_comparisons_with_count(vec: Vec<i32>, count: u32) -> (Vec<i32>, u32) {
+    if vec.len() <= 1 {
         return (vec, count)
     }
 
-    let total = count + vec.len() - 1us;
+    let total = count + vec.len() as u32 - 1;
 
     let result = median_partition(vec);
 
@@ -115,8 +122,8 @@ pub fn count_median_comparisons_with_count(vec: Vec<usize>, count: usize) -> (Ve
     (left, count2)
 }
 
-fn median_partition(og_vec: Vec<usize>) -> Partition {
-    if og_vec.len() <= 1us {
+fn median_partition(og_vec: Vec<i32>) -> Partition {
+    if og_vec.len() <= 1 {
         return Partition {
             left: Vec::new(),
             pivot: og_vec,
@@ -124,12 +131,12 @@ fn median_partition(og_vec: Vec<usize>) -> Partition {
         }
     }
 
-    let mut vec: Vec<usize> = og_vec.to_owned();
+    let mut vec: Vec<i32> = og_vec.to_owned();
 
-    // let mut pivot = 0us;
-    let first = vec[0us];
+    // let mut pivot = 0;
+    let first = vec[0];
     let last = *vec.last().unwrap();
-    let middle_idx = (vec.len() as f32/2f32).ceil() as usize - 1us;
+    let middle_idx = (vec.len() as f32/2f32).ceil() as usize - 1;
     let middle = vec[middle_idx];
 
     let mut pivot =
@@ -138,42 +145,42 @@ fn median_partition(og_vec: Vec<usize>) -> Partition {
         } else if last <= middle && middle <= first {
             middle_idx
         } else if last <= first && first <= middle {
-            0us
+            0
         } else if middle <= first && first <= last {
-            0us
+            0
         } else { // last
-            vec.len() - 1us
+            vec.len() - 1
         };
 
-    vec.swap(0us, pivot);
+    vec.swap(0, pivot);
 
-    pivot = 0us;
-    let mut i = 1us;
-    let mut j = 1us;
+    pivot = 0;
+    let mut i = 1;
+    let mut j = 1;
     while j < vec.len() {
         if vec[j] < vec[pivot] {
             if i != j {
                 vec.swap(i, j);
             }
-            j += 1us;
-            i += 1us;
+            j += 1;
+            i += 1;
         } else {
-            j += 1us;
+            j += 1;
         }
     }
-    vec.swap(pivot, i - 1us);
+    vec.swap(pivot, i - 1);
 
     return Partition {
-        pivot: vec![vec[i-1us]],
-        left: vec.iter().enumerate().filter(|&(idx, &_n)| idx < i - 1us).map(|(_idx, &n)| n).collect(),
-        right: vec.iter().enumerate().filter(|&(idx, &_n)| idx > i - 1us).map(|(_idx, &n)| n).collect(),
+        pivot: vec![vec[i-1]],
+        left: vec.iter().enumerate().filter(|&(idx, &_n)| idx < i - 1).map(|(_idx, &n)| n).collect(),
+        right: vec.iter().enumerate().filter(|&(idx, &_n)| idx > i - 1).map(|(_idx, &n)| n).collect(),
     }
 }
 
 struct Partition {
-    left: Vec<usize>,
-    pivot: Vec<usize>,
-    right: Vec<usize>,
+    left: Vec<i32>,
+    pivot: Vec<i32>,
+    right: Vec<i32>,
 }
 
 #[cfg(test)]
@@ -187,86 +194,86 @@ mod test {
 
     #[test]
     pub fn with_no_pivot_specified_it_sorts_an_input_vector_of_10_integers() {
-        let vec = vec![8us, 3, 2, 10, 6, 5, 1, 9, 4, 7];
-        let expected = (1us..11us).collect();
+        let vec = vec![8, 3, 2, 10, 6, 5, 1, 9, 4, 7];
+        let expected = (1..11).collect();
         assert_eq!(sort(vec), expected);
     }
 
     #[test]
     pub fn with_no_pivot_specified_it_sorts_an_input_vector_of_100_integers() {
-        let mut vec: Vec<usize> = (1us..101us).collect();
+        let mut vec: Vec<i32> = (1..101).collect();
         thread_rng().shuffle(&mut vec);
-        let expected = (1us..101us).collect();
+        let expected = (1..101).collect();
         assert_eq!(sort(vec), expected);
     }
 
     #[test]
     pub fn with_no_pivot_specified_it_sorts_an_input_vector_of_1000_integers() {
-        let mut vec: Vec<usize> = (1us..1001us).collect();
+        let mut vec: Vec<i32> = (1..1001).collect();
         thread_rng().shuffle(&mut vec);
-        let expected = (1us..1001us).collect();
+        let expected = (1..1001).collect();
         assert_eq!(sort(vec), expected);
     }
 
     #[test]
     pub fn with_a_custom_pivot_it_sorts_an_input_vector_of_1000_integers() {
-        let mut vec: Vec<usize> = (1us..1001us).collect();
+        let mut vec: Vec<i32> = (1..1001).collect();
         thread_rng().shuffle(&mut vec);
-        let expected = (1us..1001us).collect();
+        let expected = (1..1001).collect();
         assert_eq!(sort(vec), expected);
     }
 
     #[test]
     pub fn with_no_custom_pivot_it_counts_comparisons() {
-        let vec = vec![2us, 1];
-        assert_eq!(count_comparisons(vec), (vec![1us, 2], 1us));
+        let vec = vec![2, 1];
+        assert_eq!(count_comparisons(vec), (vec![1, 2], 1));
     }
 
     #[test]
     pub fn with_a_custom_pivot_it_counts_comparisons() {
-        let vec = vec![2us, 1];
-        assert_eq!(count_comparisons_with_pivot(vec, 1is), (vec![1us, 2], 1us));
+        let vec = vec![2, 1];
+        assert_eq!(count_comparisons_with_pivot(vec, 1), (vec![1, 2], 1));
     }
 
     #[test]
     pub fn count_median_comparisons_with_ten_txt_test_input() {
         let mut file = BufferedReader::new(File::open(&Path::new("ten.txt")));
-        let vec: Vec<usize> = file.lines().enumerate().map(|(i, x)| {
+        let vec: Vec<i32> = file.lines().enumerate().map(|(i, x)| {
             x.unwrap()
             .trim()
-            .parse::<usize>()
+            .parse::<i32>()
             .unwrap_or_else(|err| {
-                panic!("Unable to parse int at line:{}: {}", i + 1us, err)
+                panic!("Unable to parse int at line:{}: {}", i + 1, err)
             })
         }).collect();
-        assert_eq!(count_median_comparisons(vec).1, 21us);
+        assert_eq!(count_median_comparisons(vec).1, 21);
     }
 
     #[test]
     pub fn count_median_comparisons_with_hundred_txt_test_input() {
         let mut file = BufferedReader::new(File::open(&Path::new("hundred.txt")));
-        let vec: Vec<usize> = file.lines().enumerate().map(|(i, x)| {
+        let vec: Vec<i32> = file.lines().enumerate().map(|(i, x)| {
             x.unwrap()
             .trim()
-            .parse::<usize>()
+            .parse::<i32>()
             .unwrap_or_else(|err| {
-                panic!("Unable to parse int at line:{}: {}", i + 1us, err)
+                panic!("Unable to parse int at line:{}: {}", i + 1, err)
             })
         }).collect();
-        assert_eq!(count_median_comparisons(vec).1, 518us);
+        assert_eq!(count_median_comparisons(vec).1, 518);
     }
 
     #[test]
     pub fn count_median_comparisons_with_thousand_txt_test_input() {
         let mut file = BufferedReader::new(File::open(&Path::new("thousand.txt")));
-        let vec: Vec<usize> = file.lines().enumerate().map(|(i, x)| {
+        let vec: Vec<i32> = file.lines().enumerate().map(|(i, x)| {
             x.unwrap()
             .trim()
-            .parse::<usize>()
+            .parse::<i32>()
             .unwrap_or_else(|err| {
-                panic!("Unable to parse int at line:{}: {}", i + 1us, err)
+                panic!("Unable to parse int at line:{}: {}", i + 1, err)
             })
         }).collect();
-        assert_eq!(count_median_comparisons(vec).1, 8921us);
+        assert_eq!(count_median_comparisons(vec).1, 8921);
     }
 }
